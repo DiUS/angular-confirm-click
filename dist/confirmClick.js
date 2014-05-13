7fdf35d663e535ad8274c0b2832ee3d5cdf7f271
azzamallow <azzamallow@gmail.com>
2014-05-12 22:45:25 -0400

    fixed ngmin configuration

--- dist/confirmClick.js
+++ dist/confirmClick.js
@@ -1,58 +1,51 @@
-(function() {
+(function () {
   'use strict';
   angular.module('confirmClick', []);
-
-  angular.module('confirmClick').directive('confirmClick', function($timeout) {
-    return {
-      scope: {},
-      link: function(scope, element, attrs) {
-        var actionText, hasConfirmed, promise, textWidth;
-        actionText = element.text();
-        textWidth = null;
-        promise = null;
-        hasConfirmed = false;
-        scope.confirmingAction = false;
-        element.css({
-          transition: 'all 1s'
-        });
-        scope.$watch('confirmingAction', function(newVal, oldVal) {
-          if ((newVal === oldVal && oldVal === false)) {
-            textWidth = element[0].offsetWidth;
-          }
-          if (scope.confirmingAction) {
-            element.text(attrs.confirmMessage);
-            return element.css({
-              maxWidth: '300px'
-            });
-          } else {
-            element.text(actionText);
-            return element.css({
-              maxWidth: textWidth
-            });
-          }
-        });
-        return element.bind('click', function() {
-          if (!scope.confirmingAction) {
-            scope.$apply(function() {
-              return scope.confirmingAction = true;
-            });
-            return promise = $timeout(function() {
-              return scope.confirmingAction = false;
-            }, 1500);
-          } else {
-            if (hasConfirmed) {
-              return;
+  angular.module('confirmClick').directive('confirmClick', [
+    '$timeout',
+    function ($timeout) {
+      return {
+        scope: {},
+        link: function (scope, element, attrs) {
+          var actionText, hasConfirmed, promise, textWidth;
+          actionText = element.text();
+          textWidth = null;
+          promise = null;
+          hasConfirmed = false;
+          scope.confirmingAction = false;
+          element.css({ transition: 'all 1s' });
+          scope.$watch('confirmingAction', function (newVal, oldVal) {
+            if (newVal === oldVal && oldVal === false) {
+              textWidth = element[0].offsetWidth;
             }
-            hasConfirmed = true;
-            $timeout.cancel(promise);
-            element.css({
-              opacity: '0.5'
-            });
-            return scope.$parent.$apply(attrs.confirmClick);
-          }
-        });
-      }
-    };
-  });
-
-}).call(this);
+            if (scope.confirmingAction) {
+              element.text(attrs.confirmMessage);
+              return element.css({ maxWidth: '300px' });
+            } else {
+              element.text(actionText);
+              return element.css({ maxWidth: textWidth });
+            }
+          });
+          return element.bind('click', function () {
+            if (!scope.confirmingAction) {
+              scope.$apply(function () {
+                return scope.confirmingAction = true;
+              });
+              return promise = $timeout(function () {
+                return scope.confirmingAction = false;
+              }, 1500);
+            } else {
+              if (hasConfirmed) {
+                return;
+              }
+              hasConfirmed = true;
+              $timeout.cancel(promise);
+              element.css({ opacity: '0.5' });
+              return scope.$parent.$apply(attrs.confirmClick);
+            }
+          });
+        }
+      };
+    }
+  ]);
+}.call(this));
